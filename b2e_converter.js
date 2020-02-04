8b7ecff8ad5fa033050a5b123509bee9a9e185d7
ulrike <ulrike.bath@live.de>
2020-02-04 12:03:56 +0100

    WIP: rewrite b2e_converter.js to parse babylon ast as well

--- b2e_converter.js
+++ b2e_converter.js
@@ -1,6 +1,5 @@
 "use strict";
 
-// TODO browserify cannot find these files
 const babylonToEspree = require("babel-eslint/lib/babylon-to-espree");
 const {
     parseSync: parse,
@@ -9,65 +8,39 @@ const {
     loadPartialConfig,
 } = require("@babel/core");
 
-module.exports = function(code, ast) {
+module.exports = function(code, options) {
+    let opts = {
+        plugins: options.plugins,
+        sourceType: options.sourceType,
+        filename: options.filePath,
+        babelrc: options.babelrc,
+        parserOpts: {
+            allowImportExportEverywhere: options.allowImportExportEverywhere, // consistent with espree
+            allowReturnOutsideFunction: true,
+            allowSuperOutsideMethod: true,
+            ranges: true,
+            tokens: true,
+            plugins: ["estree"],
+        },
+        caller: {
+            name: "babel-eslint",
+        },
+    };
 
+    let ast;
+
+    try {
+        ast = parse(code, opts);
+    } catch (err) {
+        if (err instanceof SyntaxError) {
+            err.lineNumber = err.loc.line;
+            err.column = err.loc.column;
+        }
+
+        throw err;
+    }
+    console.log(JSON.stringify(ast));
     babylonToEspree(ast, traverse, tt, code);
-
-    ast = addRanges(ast);
 
     return ast;
 };
-
-function addRanges(ast) {
-    if (ast.body) {
-        if(ast.body.length > 0) {
-            ast.body.forEach((element, index) => {
-                ast.body[index] = addRanges(element);
-            })
-        }else {
-            ast.body = addRanges(ast.body);
-        }
-    }
-    if(ast.declarations && ast.declarations.length > 0) {
-        ast.declarations.forEach( (element, index) => {
-            ast.declarations[index] = addRanges(element);
-        })
-    }
-    if(ast.params && ast.params.length > 0) {
-        ast.params.forEach( (element, index) => {
-            ast.params[index] = addRanges(element);
-        })
-    }
-    if(ast.id) {
-        ast.id = addRanges(ast.id);
-    }
-    if(ast.init) {
-        ast.init = addRanges(ast.init);
-    }
-    if(ast.arguments && ast.arguments.length > 0) {
-        ast.arguments.forEach( (element, index) => {
-            ast.arguments[index] = addRanges(element);
-        })
-    }
-    if(ast.argument) {
-        ast.argument = addRanges(ast.argument);
-        ast.argument.left = addRanges(ast.argument.left);
-        ast.argument.right = addRanges(ast.argument.right)
-    }
-    if(ast.expression) {
-        ast.expression = addRanges(ast.expression);
-    }
-    if(ast.callee) {
-        ast.callee = addRanges(ast.callee);
-    }
-    if(ast.object) {
-        ast.object = addRanges(ast.object);
-    }
-
-    if(ast.start && ast.end) {
-        ast.range = [ast.start, ast.end];
-    }else if (!ast.range){
-        ast.range = [0, 0];
-    }
-    return ast;
-}
--- test_parser.js
+++ test_parser.js
@@ -2,5 +2,5 @@ const b2e_converter = require('./b2e_converter');
 const parse = require('babel-eslint/lib/parse')
 const ast = require('./ast_source');
 
-let newAST = b2e_converter("var a = 3;", ast)
+let newAST = b2e_converter("var a = { b:3, c:4};", {})
 console.log(JSON.stringify(newAST));
